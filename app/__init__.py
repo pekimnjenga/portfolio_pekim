582e23600813727720d326ff0bd96a7965018134
pekimnjenga <njengapekim@gmail.com>
2026-04-24 20:53:44 +0300

    update vercel.json routing for static file delivery

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
+import os
 from flask import Flask
 from app.routes import main
 
 def create_app(config=None):
-    app = Flask(__name__, static_folder="static", static_url_path='/static', template_folder="templates")
+    # Get the absolute path to the directory containing __init__.py (which is 'app')
+    app_dir = os.path.abspath(os.path.dirname(__file__))
+    # Construct the absolute paths to the static and templates folders
+    static_folder_path = os.path.join(app_dir, 'static')
+    template_folder_path = os.path.join(app_dir, 'templates')
+
+    app = Flask(__name__,
+                static_folder=static_folder_path,
+                static_url_path='/static',
+                template_folder=template_folder_path)
 
     # Register blueprints
     app.register_blueprint(main)
